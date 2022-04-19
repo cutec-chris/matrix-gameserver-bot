75ab78c12620d2ed0ca8f8d7e4d0c958b4e6664b
Christian U <christian@ullihome.de>
2022-04-19 20:10:20 +0200

    move Config to class, load servers once

--- source/bot.py
+++ source/bot.py
@@ -1,24 +1,26 @@
 from init import *
 import rcon,a2s
-servers = []
 loop = None
 lastsend = None
+class Server(Config):
+    def __init__(self, room, **kwargs) -> None:
+        super().__init__(room, **kwargs)
 @bot.listener.on_message_event
 async def tell(room, message):
     global servers,lastsend
     match = botlib.MessageMatch(room, message, bot, prefix)
     if match.is_not_from_this_bot() and match.prefix()\
     and match.command("listen"):
-        server = {
+        server = Server({
             'room': room.room_id,
             'server': match.args()[1],
             'rcon': match.args()[2],
             'password': None
-        }
+        })
         if len(match.args())>3:
-            server['password'] = match.args()[3]
+            server.password = match.args()[3]
         if len(match.args())>4:
-            server['qport'] = match.args()[4]
+            server.qport = match.args()[4]
         servers.append(server)
         loop.create_task(check_server(server))
         with open('data.json', 'w') as f:
@@ -26,43 +28,39 @@ async def tell(room, message):
         await bot.api.send_text_message(room.room_id, 'ok')
     elif match.is_not_from_this_bot() and match.prefix():
         for server in servers:
-            if server['room'] == room.room_id and '_client' in server:
+            if server.room == room.room_id and '_client' in server:
                 break
-        if server['room'] != room.room_id: return
+        if server.room != room.room_id: return
         ncmd = " ".join(arg for arg in match.args())
-        if not ('admins' in server and message.sender in server['admins']):
+        if not ('admins' in server and message.sender in server.admins):
             await bot.api.send_text_message(room.room_id, 'not authorized')
         try:
-            res = server['_client'].run(ncmd)
+            res = server._client.run(ncmd)
             if not 'Server received, But no response!!' in res:
                 await bot.api.send_text_message(room.room_id, res)
         except BaseException as e:
             await bot.api.send_text_message(room.room_id, str(e))
     elif match.is_not_from_this_bot():
         for server in servers:
-            if server['room'] == room.room_id and '_client' in server:
+            if server.room == room.room_id and '_client' in server:
                 break
-        if server['room'] != room.room_id: return
+        if server.room != room.room_id: return
         try:
             user = message.sender
             user = user[:user.find(':')]
             if user[:1] == '@':
                 user = user[1:]
-            server['_client'].run('ServerChat',user+':'+message.body)
-            server['_client'].run('amx_say',user+':'+message.body)
+            server._client.run('ServerChat',user+':'+message.body)
+            server._client.run('amx_say',user+':'+message.body)
             lastsend = user+':'+message.body
         except:
             pass
 async def check_server(server):
     global lastsend,servers
-    def update_server_var():
-        for server_r in servers:
-            if server_r['room'] == server['room']:
-                server_r = server
     while True:
         try:
-            with rcon.source.Client(server['server'], int(server['rcon']), passwd=server['password']) as client:
-                server['_client'] = client
+            with rcon.source.Client(server.server, int(server.rcon), passwd=server.password) as client:
+                server._client = client
                 update_server_var()
                 def tell(*args):
                     res = client.run(*args)
@@ -71,25 +69,25 @@ async def check_server(server):
                         return res
                     return None
                 try:
-                    info = a2s.info((server['server'], int(server['qport'])))
+                    info = a2s.info((server.server, int(server.qport)))
                     answer = 'Server %s is up with %s ...\nMap: %s\nPlayers: %d of %d' % (info.server_name,info.game,info.map_name,info.player_count,info.max_players)
                 except BaseException as e:
                     info = None
                     answer = 'Server is up now...'
-                await bot.api.send_text_message(server['room'],answer)
+                await bot.api.send_text_message(server.room,answer)
                 while True:
                     res = None
-                    if 'gamelog' in server and server['gamelog']:
+                    if 'gamelog' in server and server.gamelog:
                         res = tell('GetGameLog')
                     if res:
                         if 'error' in res:
-                            server['gamelog'] = False
+                            server.gamelog = False
                             update_server_var()
                         res = res[res.find(':')+1:].rstrip()
                         if 'SERVER:' in res:
                             res = res[res.find(':')+1:]
                         if not lastsend or (not (lastsend in res)):
-                            await bot.api.send_text_message(server['room'],res)
+                            await bot.api.send_text_message(server.room,res)
                         else:
                             lastsend = None
                     else:
@@ -97,18 +95,22 @@ async def check_server(server):
         except BaseException as e:
             if 'Connection' in str(e): pass
             else:
-                await bot.api.send_text_message(server['room'],str(e))
+                await bot.api.send_text_message(server.room,str(e))
         await asyncio.sleep(5)
+try:
+    with open('data.json', 'r') as f:
+        nservers = json.load(f)
+        for server in nservers:
+            servers.append(Server(server))
+except BaseException as e: 
+    logging.error('Failed to read config.yml:'+str(e))
+    exit(1)
 @bot.listener.on_startup
 async def startup(room):
     global loop,servers
     loop = asyncio.get_running_loop()
-    try:
-        with open('data.json', 'r') as f:
-            servers = json.load(f)
-    except: pass
     for server in servers:
-        if server['room'] == room:
+        if server.room == room:
             loop.create_task(check_server(server))
 @bot.listener.on_message_event
 async def bot_help(room, message):
--- source/init.py
+++ source/init.py
@@ -1,4 +1,4 @@
-import simplematrixbotlib as botlib,yaml,json,asyncio
+import simplematrixbotlib as botlib,yaml,json,logging,asyncio,nio
 with open('config.yml', 'r') as file:
     config = yaml.safe_load(file)
 try: 
@@ -7,3 +7,51 @@ except:
     prefix = config['server']['user']
 creds = botlib.Creds(config['server']['url'], config['server']['user'], config['server']['password'])
 bot = botlib.Bot(creds)
+class Config(object):
+    def __init__(self,room,**kwargs) -> None:
+        if isinstance(room, dict):
+            self.__dict__.update(room)
+        else:
+            self.room = room
+            self.__dict__.update(kwargs)
+loop = None
+servers = []
+async def save_servers():
+    global servers
+    sservers = []
+    for server in servers:
+        ndict = {k: v for k, v in server.__dict__.items() if not k.startswith('_')}
+        sservers.append(ndict)
+    with open('data.json', 'w') as f:
+        json.dump(sservers,f, skipkeys=True, indent=4)
+def is_valid_event(event):
+    events = (nio.RoomMessageFormatted, nio.RedactedEvent)
+    events += (nio.RoomMessageMedia, nio.RoomEncryptedMedia)
+    return isinstance(event, events)
+async def fetch_room_events(
+    client,
+    start_token: str,
+    room,
+    direction,
+    limit
+) -> list:
+    events = []
+    while len(events)<limit:
+        response = await client.room_messages(
+            room.room_id, start_token, limit=10, direction=direction
+        )
+        if len(response.chunk) == 0:
+            break
+        events.extend(event for event in response.chunk if is_valid_event(event))
+        start_token = response.end
+    return events
+async def get_room_events(client, room, limit = 1):
+    sync_resp = await client.sync(
+        full_state=True, sync_filter={"room": {"timeline": {"limit": limit}}}
+    )
+    start_token = sync_resp.rooms.join[room].timeline.prev_batch
+    # Generally, it should only be necessary to fetch back events but,
+    # sometimes depending on the sync, front events need to be fetched
+    # as well.
+    events = await fetch_room_events(client,start_token,bot.api.async_client.rooms[room],nio.MessageDirection.back,limit)
+    return events
